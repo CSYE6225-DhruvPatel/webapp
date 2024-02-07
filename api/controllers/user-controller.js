87026013413dbf29124d71724536e4a976fe2ef7
Dhruv Patel <patel.dhruvr@northeastern.edu>
2024-02-07 15:53:23 -0500

    added user endpoints

---
+++ api/controllers/user-controller.js
@@ -0,0 +1,29 @@
+import * as userService from "../services/user-service.js";
+import { sendResponse } from "../utils/response.js";
+
+export const create = async (req, res) => {
+    try {
+        const user = await userService.createUser(req.body);
+        sendResponse({ req, res, status: 201, data: user});
+    } catch (err) {
+        sendResponse({ req, res, status: 400});
+    }
+}
+
+export const get = async (req, res) => {
+    try {
+        const user = await userService.getUserByName(req.body.username);
+        sendResponse({ req, res, status: 200, data: user});
+    } catch (err) {
+        sendResponse({ req, res, status: 400});
+    }
+}
+
+export const put = async (req, res) => {
+    try {
+        await userService.updateUser(req.user.username, req.body);
+        sendResponse({ req, res, status: 204});
+    } catch (err) {
+        sendResponse({ req, res, status: 400});
+    }
+}
