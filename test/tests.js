93cb2ef7d54661dc4c421541c3c6dbaf350ddc8a
Dhruv Patel <patel.dhruvr@northeastern.edu>
2024-02-08 12:50:35 -0500

    edited workflow with rebase

---
+++ test/tests.js
@@ -0,0 +1,17 @@
+const { alwaysTrue, alwaysFalse } = require('./tests');
+
+test('alwaysTrue function returns true', () => {
+  expect(alwaysTrue()).toBe(true);
+});
+
+test('alwaysFalse function returns false', () => {
+  expect(alwaysFalse()).toBe(false);
+});
+
+function alwaysTrue() {
+  return true;
+}
+
+function alwaysFalse() {
+  return false;
+}
