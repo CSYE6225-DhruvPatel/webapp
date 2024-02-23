f56f94c87647e1f39eed0b8e0e2612c98b552100
Dhruv Patel <patel.dhruvr@northeastern.edu>
2024-02-22 19:12:03 -0500

    packer build corrected

--- packer/install-script.sh
+++ packer/install-script.sh
@@ -6,6 +6,13 @@ sudo dnf clean all -y
 sudo dnf install unzip -y
 sudo sudo yum install -y unzip
 
+chmod +x /dhruv/install-script.sh
+/dhruv/install-script.sh
+chmod +x /dhruv/postgres.sh
+/dhruv/postgres.sh
+chmod +x /dhruv/setup-npm.sh
+/dhruv/setup-npm.sh
+
 echo '---Install nodeJs'
 sudo dnf module reset nodejs
 sudo dnf -y module enable nodejs:20
@@ -19,3 +26,11 @@ sudo postgresql-setup --initdb
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
+PG_HBA_PATH="/var/lib/pgsql/data/pg_hba.conf"
+sudo sed -i 's/ident/trust/g' "$PG_HBA_PATH"
+sudo sed -i 's/peer/trust/g' "$PG_HBA_PATH"
+
+sudo systemctl restart postgresql
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+sudo -u postgres psql
+echo "PostgreSQL configured successfully"
